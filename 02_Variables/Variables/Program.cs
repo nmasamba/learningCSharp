4d8db1ab18f795f05640ac1de8162d76faba9608
nmasamba <slimm4rl@yahoo.com>
2017-02-11 20:07:50 +0000

    variables

---
+++ 02_Variables/Variables/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            
+            var myName = "Nyasha Masamba - vamwe vanoti Pride";
+
+            Console.WriteLine(myName);
+            */
+
+            
+            int x = 7;
+            // string y = "Nyasha";
+            string y = "5";
+
+            /*
+            string myFirstTry = x.ToString() + y;
+
+            Console.WriteLine(myFirstTry);
+            */
+
+
+            int mySecondTry = x + int.Parse(y);
+
+            Console.WriteLine(mySecondTry);
+            Console.ReadLine();
+
+        }
+    }
+}
