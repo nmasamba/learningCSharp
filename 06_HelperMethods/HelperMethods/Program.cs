bc9582ca13417b9e1fbdf29f2387e1effc8323ba
nmasamba <slimm4rl@yahoo.com>
2017-02-19 01:38:46 +0000

    helper methods

---
+++ 06_HelperMethods/HelperMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myValue = superSecretFormula("coder!");
+            Console.WriteLine(myValue);
+            Console.ReadLine();
+        }
+
+        private static string superSecretFormula(string name)
+        {
+            //insert cool stuff here
+            return String.Format("Hello, {0}!", name);
+        }
+
+        //Overloading
+        private static string superSecretFormula()
+        {
+            //insert cool stuff again
+            return "Hello World";
+        }
+
+
+    }
+}
